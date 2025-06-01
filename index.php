4bfde5b5d5270f441147587bc6ea5fe8d262df1f
Gab-off <albuquerque1.gabriel@gmail.com>
2025-06-01 16:50:51 -0300

    Insert bar and button finished

--- index.php
+++ index.php
@@ -14,27 +14,28 @@ require_once 'handle_post.php';
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>To-do-list</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
 <div class="container">
     <form id="taskForm" action="index.php" method="POST">
-        Task: <input id="taskText" type="text" name="task" required>
-        <input type="radio" id="baixa" name="priority_task" value="low" checked>
-        <label for="baixa">baixa</label>
-        <input type="radio" id="media" name="priority_task" value="medium">
-        <label for="media">média</label>
-        <input type="radio" id="alta" name="priority_task" value="high">
-        <label for="alta">alta</label>
-        <br>
-        <br>
-        <div class="buttons-add">
-            <input type="submit" name="submit" value="Add">
-            <input type="submit" name="reset" value="Reset">
-        </div>
+        <input id="taskText" type="text" name="task" placeholder="Insert your task here">
+<!--        TODO: verificar onde colocar isso na hora que a task vai para a view-->
+<!--        <div class="priority-level">-->
+<!--            <input type="radio" id="baixa" name="priority_task" value="low" checked>-->
+<!--            <label for="baixa">baixa</label>-->
+<!--            <input type="radio" id="media" name="priority_task" value="medium">-->
+<!--            <label for="media">média</label>-->
+<!--            <input type="radio" id="alta" name="priority_task" value="high">-->
+<!--            <label for="alta">alta</label>-->
+<!--        </div>-->
+            <input id="submitButton" type="submit" name="submit" value="ADD">
+
+<!--            TODO: retirar o submit-->
+<!--            <input type="submit" name="reset" value="Reset">-->
+    </form>
         <br>
         <ul>
-    </form>
             <?php foreach ($_SESSION['tasks'] as $index => $task): ?>
                 <li><div class="task-item">
                         <p class="item <?= showTaskPriority($task["prioridade"]) ?> <?= $task['concluida']? 'concluido' : ''?>"><?= htmlspecialchars($task['tarefa'])  ?></p>
